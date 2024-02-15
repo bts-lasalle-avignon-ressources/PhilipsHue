246a548702f2f45e3b885def210537bfccfba419
Thierry Vaira <tvaira@free.fr>
2024-02-15 16:32:27 +0100

    Finalise exemple Android

--- src/android/MyApplicationHTTP/app/src/main/java/com/example/myapplicationhttp/MainActivity.java
+++ src/android/MyApplicationHTTP/app/src/main/java/com/example/myapplicationhttp/MainActivity.java
@@ -12,6 +12,7 @@ import okhttp3.RequestBody;
 
 // JSON (JavaScript Object Notation) est un format d'échange de données textuelles
 // https://developer.android.com/reference/org/json/package-summary
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONException;
 
@@ -29,17 +30,24 @@ import android.widget.TextView;
 import java.io.IOException;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity
 {
     private final String TAG               = "ClientHTTP";
     private OkHttpClient clientOkHttp      = null;
-    private String       url               = "https://192.168.52.182/clip/v2/resource/light";
+    private String       adresseIPPontHue  = "";
+    private String       url               = null;
     private String       hueApplicationKey = "8FcG-JAXZa47KLLtzjmYlDp73nOBrUJ1ktbrmtvf";
+    private Button       boutonDecouvrir;
     private Button       boutonEnvoyer;
+    private Button       boutonEteindre;
+    private Button       boutonAllumer;
     private TextView     urlRequete;
     private TextView     reponseEtat;
     private TextView     reponseJson;
+    private List<String> idEclairages = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -60,17 +68,83 @@ public class MainActivity extends AppCompatActivity
             throw new RuntimeException(e);
         }
 
-        urlRequete = (TextView)findViewById(R.id.urlRequete);
-        urlRequete.setText(url);
-        reponseEtat   = (TextView)findViewById(R.id.reponseEtat);
-        reponseJson   = (TextView)findViewById(R.id.reponseJson);
+        urlRequete      = (TextView)findViewById(R.id.urlRequete);
+        reponseEtat     = (TextView)findViewById(R.id.reponseEtat);
+        reponseJson     = (TextView)findViewById(R.id.reponseJson);
+        idEclairages    = new ArrayList<String>();
+        boutonDecouvrir = (Button)findViewById(R.id.boutonDecouvrir);
+        boutonDecouvrir.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                if(estConnecteReseau())
+                {
+                    url = "https://discovery.meethue.com/";
+                    decouvrirPontHue(url);
+                }
+                else
+                {
+                    reponseEtat.setText("Aucune connexion réseau !");
+                }
+            }
+        });
         boutonEnvoyer = (Button)findViewById(R.id.boutonEnvoyer);
+        boutonEnvoyer.setEnabled(false);
         boutonEnvoyer.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v)
             {
                 if(estConnecteReseau())
                 {
-                    emettreRequeteOkHttp(url, hueApplicationKey);
+                    if(!adresseIPPontHue.isEmpty())
+                    {
+                        url = "https://" + adresseIPPontHue + "/clip/v2/resource/light";
+                        listerEclairages(url);
+                    }
+                }
+                else
+                {
+                    reponseEtat.setText("Aucune connexion réseau !");
+                }
+            }
+        });
+        boutonEteindre = (Button)findViewById(R.id.boutonEteindre);
+        boutonEteindre.setEnabled(false);
+        boutonEteindre.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                if(estConnecteReseau())
+                {
+                    if(!adresseIPPontHue.isEmpty())
+                    {
+                        for(int i = 0; i < idEclairages.size(); i++)
+                        {
+                            String id = idEclairages.get(i);
+                            url = "https://" + adresseIPPontHue + "/clip/v2/resource/light/" + id;
+                            eteindreEclairage(url);
+                        }
+                    }
+                }
+                else
+                {
+                    reponseEtat.setText("Aucune connexion réseau !");
+                }
+            }
+        });
+        boutonAllumer = (Button)findViewById(R.id.boutonAllumer);
+        boutonAllumer.setEnabled(false);
+        boutonAllumer.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                if(estConnecteReseau())
+                {
+                    if(!adresseIPPontHue.isEmpty())
+                    {
+                        for(int i = 0; i < idEclairages.size(); i++)
+                        {
+                            String id = idEclairages.get(i);
+                            url = "https://" + adresseIPPontHue + "/clip/v2/resource/light/" + id;
+                            allumerEclairage(url);
+                        }
+                    }
                 }
                 else
                 {
@@ -80,23 +154,22 @@ public class MainActivity extends AppCompatActivity
         });
     }
 
-    private void emettreRequeteOkHttp(String url, String hueApplicationKey)
+    private void decouvrirPontHue(String url)
     {
         if(clientOkHttp == null)
             return;
-        Log.d(TAG, "emettreRequeteOkHttp()");
-        Request request = new Request.Builder()
-                            .url(url)
-                            .addHeader("Content-Type", "application/json")
-                            .addHeader("hue-application-key", hueApplicationKey)
-                            .build();
+        Log.d(TAG, "decouvrirPontHue() url = " + url);
+        urlRequete.setText(url);
+
+        Request request =
+          new Request.Builder().url(url).addHeader("Content-Type", "application/json").build();
 
         clientOkHttp.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(Call call, IOException e)
             {
                 e.printStackTrace();
-                Log.d("OkHttp", "onFailure");
+                Log.d(TAG, "decouvrirPontHue() onFailure");
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run()
@@ -109,8 +182,8 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onResponse(Call call, Response response) throws IOException
             {
-                Log.d("OkHttp", "onResponse - message = " + response.message());
-                Log.d("OkHttp", "onResponse - code    = " + response.code());
+                Log.d(TAG, "decouvrirPontHue() onResponse - message = " + response.message());
+                Log.d(TAG, "decouvrirPontHue() onResponse - code    = " + response.code());
 
                 if(!response.isSuccessful())
                 {
@@ -130,28 +203,288 @@ public class MainActivity extends AppCompatActivity
                         reponseJson.setText(body);
 
                         /*
-                            {"power":0,"Ws":0,"relay":true,"temperature":21.5}
+                        [
+                            {
+                                "id":"ecb5fafffe01c1b5",
+                                "internalipaddress":"192.168.52.16",
+                                "port":443
+                            }
+                        ]
                         */
-                        /*JSONObject json = null;
+                        JSONArray json = null;
 
                         try
                         {
-                            json = new JSONObject(body);
-                            //JSONObject payloadFields = null;
-                            //payloadFields = json.getJSONObject("main");
-                            double power = json.getDouble("power");
-                            double Ws = json.getDouble("Ws");
-                            Boolean relay = json.getBoolean("relay");
-                            double temperature = json.getDouble("temperature");
-                            Log.d("OkHttp", "Power       = " + power);
-                            Log.d("OkHttp", "Ws          = " + Ws);
-                            Log.d("OkHttp", "Etat        = " + relay);
-                            Log.d("OkHttp", "Température = " + temperature + " °C");
+                            json                     = new JSONArray(body);
+                            JSONObject payloadFields = null;
+                            payloadFields            = json.getJSONObject(0);
+                            adresseIPPontHue         = payloadFields.getString("internalipaddress");
+                            Log.d(TAG, "decouvrirPontHue() adresseIPPontHue = " + adresseIPPontHue);
                         }
-                        catch (JSONException e)
+                        catch(JSONException e)
                         {
                             e.printStackTrace();
-                        }*/
+                        }
+
+                        if(!adresseIPPontHue.isEmpty())
+                        {
+                            boutonEnvoyer.setEnabled(true);
+                        }
+                        else
+                        {
+                            boutonEnvoyer.setEnabled(false);
+                            boutonEteindre.setEnabled(false);
+                            boutonAllumer.setEnabled(false);
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+    private void listerEclairages(String url)
+    {
+        if(clientOkHttp == null)
+            return;
+        Log.d(TAG, "listerEclairages()");
+        Log.d(TAG, "listerEclairages() url = " + url);
+        urlRequete.setText(url);
+        Request request = new Request.Builder()
+                            .url(url)
+                            .addHeader("Content-Type", "application/json")
+                            .addHeader("hue-application-key", hueApplicationKey)
+                            .build();
+
+        clientOkHttp.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e)
+            {
+                e.printStackTrace();
+                Log.d(TAG, "listerEclairages() onFailure");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run()
+                    {
+                        reponseEtat.setText("Erreur requête OkHttp !");
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException
+            {
+                Log.d(TAG, "listerEclairages() onResponse - message = " + response.message());
+                Log.d(TAG, "listerEclairages() onResponse - code    = " + response.code());
+
+                if(!response.isSuccessful())
+                {
+                    throw new IOException(response.toString());
+                }
+
+                final String body = response.body().string();
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run()
+                    {
+                        if(!response.message().isEmpty())
+                            reponseEtat.setText(response.message());
+                        else
+                            reponseEtat.setText(String.valueOf(response.code()));
+                        reponseJson.setText(body);
+
+                        /*
+                            {
+                                "errors": [],
+                                "data": [
+                                    {
+                                        "id": "5d4ba384-f49c-45a1-a68f-874a5008b967",
+                                        "id_v1": "/lights/3",
+                                        ...
+                                        "type": "light"
+                                    },
+                                    {
+                                        "id": "a40100ed-eedf-4f8b-b5f9-271b790edb2f",
+                                        "id_v1": "/lights/2",
+                                        ...
+                                        "type": "light"
+                                    },
+                                    {
+                                        "id": "f4d936da-441d-46d5-bf58-0089ad4a9aa9",
+                                        "id_v1": "/lights/1",
+                                        ...
+                                        "type": "light"
+                                    }
+                                ]
+                            }
+                        */
+                        JSONObject json = null;
+
+                        try
+                        {
+                            json           = new JSONObject(body);
+                            JSONArray data = json.getJSONArray("data");
+
+                            idEclairages.clear();
+                            for(int i = 0; i < data.length(); i++)
+                            {
+                                JSONObject eclairage = data.getJSONObject(i);
+                                String     id        = eclairage.getString("id");
+                                Log.d(TAG,
+                                      "listerEclairages() id = " + id +
+                                        " - type = " + eclairage.getString("type"));
+                                idEclairages.add(id);
+                            }
+                        }
+                        catch(JSONException e)
+                        {
+                            e.printStackTrace();
+                        }
+
+                        if(idEclairages.size() > 0)
+                        {
+                            boutonEteindre.setEnabled(true);
+                            boutonAllumer.setEnabled(true);
+                        }
+                        else
+                        {
+                            boutonEteindre.setEnabled(false);
+                            boutonAllumer.setEnabled(false);
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+    private void eteindreEclairage(String url)
+    {
+        if(clientOkHttp == null)
+            return;
+        Log.d(TAG, "eteindreEclairage()");
+        Log.d(TAG, "eteindreEclairage() url = " + url);
+        urlRequete.setText(url);
+
+        MediaType   JSON     = MediaType.parse("application/json; charset=utf-8");
+        String      jsonBody = "{\"on\":{\"on\": false}}";
+        RequestBody body     = RequestBody.create(JSON, jsonBody);
+
+        Request request = new Request.Builder()
+                            .url(url)
+                            .put(body)
+                            .addHeader("Content-Type", "application/json")
+                            .addHeader("Accept", "application/json")
+                            .addHeader("hue-application-key", hueApplicationKey)
+                            .build();
+
+        clientOkHttp.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e)
+            {
+                e.printStackTrace();
+                Log.d(TAG, "eteindreEclairage() onFailure");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run()
+                    {
+                        reponseEtat.setText("Erreur requête OkHttp !");
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException
+            {
+                Log.d(TAG, "eteindreEclairage() onResponse - message = " + response.message());
+                Log.d(TAG, "eteindreEclairage() onResponse - code    = " + response.code());
+
+                if(!response.isSuccessful())
+                {
+                    throw new IOException(response.toString());
+                }
+
+                final String body = response.body().string();
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run()
+                    {
+                        if(!response.message().isEmpty())
+                            reponseEtat.setText(response.message());
+                        else
+                            reponseEtat.setText(String.valueOf(response.code()));
+                        reponseJson.setText(body);
+
+                        /*
+                            {"data":[{"rid":"f4d936da-441d-46d5-bf58-0089ad4a9aa9","rtype":"light"}],"errors":[]}
+                        */
+                    }
+                });
+            }
+        });
+    }
+
+    private void allumerEclairage(String url)
+    {
+        if(clientOkHttp == null)
+            return;
+        Log.d(TAG, "allumerEclairage()");
+        Log.d(TAG, "allumerEclairage() url = " + url);
+        urlRequete.setText(url);
+
+        MediaType   JSON     = MediaType.parse("application/json; charset=utf-8");
+        String      jsonBody = "{\"on\":{\"on\": true}}";
+        RequestBody body     = RequestBody.create(JSON, jsonBody);
+
+        Request request = new Request.Builder()
+                            .url(url)
+                            .put(body)
+                            .addHeader("Content-Type", "application/json")
+                            .addHeader("Accept", "application/json")
+                            .addHeader("hue-application-key", hueApplicationKey)
+                            .build();
+
+        clientOkHttp.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e)
+            {
+                e.printStackTrace();
+                Log.d(TAG, "eteindreEclairage() onFailure");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run()
+                    {
+                        reponseEtat.setText("Erreur requête OkHttp !");
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException
+            {
+                Log.d(TAG, "eteindreEclairage() onResponse - message = " + response.message());
+                Log.d(TAG, "eteindreEclairage() onResponse - code    = " + response.code());
+
+                if(!response.isSuccessful())
+                {
+                    throw new IOException(response.toString());
+                }
+
+                final String body = response.body().string();
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run()
+                    {
+                        if(!response.message().isEmpty())
+                            reponseEtat.setText(response.message());
+                        else
+                            reponseEtat.setText(String.valueOf(response.code()));
+                        reponseJson.setText(body);
+
+                        /*
+                            {"data":[{"rid":"f4d936da-441d-46d5-bf58-0089ad4a9aa9","rtype":"light"}],"errors":[]}
+                        */
                     }
                 });
             }
